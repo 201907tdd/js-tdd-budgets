872398ec4b16b97c5ed8d6f535452a1ce3e9b90f
Joey Chen <jay.hatelove@gmail.com>
2020-10-24 12:27:11 +0800

    refactor test: move sut to beforeEach() and extract method totalAmountShouldBe()

--- test/budget_manager.test.js
+++ test/budget_manager.test.js
@@ -1,11 +1,20 @@
 import {BudgetManager} from "../src/budget_manager";
 
 describe('total amount between period', function () {
-    it('no budgets', () => {
-        let budgetManager = new BudgetManager();
-        const start = new Date(2000, 3, 1);
-        const end = new Date(2000, 3, 1);
-        const totalAmount = budgetManager.totalAmount(start, end);
-        expect(totalAmount).toBe(0);
+    let budgetManager;
+    beforeEach(() => {
+        budgetManager = new BudgetManager();
     });
+
+    it('no budgets', () => {
+        totalAmountShouldBe(
+            new Date(2000, 3, 1),
+            new Date(2000, 3, 1),
+            0);
+    });
+
+    function totalAmountShouldBe(start, end, expected) {
+        const totalAmount = budgetManager.totalAmount(start, end);
+        expect(totalAmount).toBe(expected);
+    }
 });
