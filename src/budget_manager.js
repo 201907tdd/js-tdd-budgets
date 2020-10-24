96fc02b29b85d874cc5119cec3cb7b6b3e333fb1
Joey Chen <jay.hatelove@gmail.com>
2020-10-24 14:06:47 +0800

    refactor: move method overlappingDays() to period class to remove feature envy

--- src/budget_manager.js
+++ src/budget_manager.js
@@ -9,23 +9,9 @@ export class BudgetManager {
             const startDate = dayjs(start);
             let budget = budgets[0];
             let period = new Period(startDate, endDate);
-            return this.overlappingDays(budget, period);
+            return period.overlappingDays(budget);
         }
         return 0;
-    }
-
-    overlappingDays(budget, period) {
-        let lastDay = budget.lastDay();
-        let firstDay = budget.firstDay();
-        if (period.startDate.isAfter(lastDay)) {
-            return 0;
-        }
-        if (period.endDate.isBefore(firstDay)) {
-            return 0;
-        }
-        let overlappingStart = period.startDate.isAfter(firstDay) ? period.startDate : firstDay;
-        let overlappingEnd = period.endDate.isBefore(lastDay) ? period.endDate : lastDay;
-        return overlappingEnd.diff(overlappingStart, 'day') + 1;
     }
 
     getBudgets() {
--- src/period.js
+++ src/period.js
@@ -6,4 +6,15 @@ export class Period {
         this.startDate = startDate;
         this.endDate = endDate;
     }
+
+    overlappingDays(budget) {
+        let lastDay = budget.lastDay();
+        let firstDay = budget.firstDay();
+        if (this.startDate.isAfter(lastDay) || this.endDate.isBefore(firstDay)) {
+            return 0;
+        }
+        let overlappingStart = this.startDate.isAfter(firstDay) ? this.startDate : firstDay;
+        let overlappingEnd = this.endDate.isBefore(lastDay) ? this.endDate : lastDay;
+        return overlappingEnd.diff(overlappingStart, 'day') + 1;
+    }
 }
