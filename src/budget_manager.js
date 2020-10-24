7c44495a70fd6e1da4253cec70fdbe7dc6d9bde9
Joey Chen <jay.hatelove@gmail.com>
2020-10-24 14:38:16 +0800

    refactor: use map().reduce() instead of forEach()

--- src/budget_manager.js
+++ src/budget_manager.js
@@ -3,23 +3,12 @@ import {Period} from "./period";
 
 export class BudgetManager {
     totalAmount(start, end) {
-        const endDate = dayjs(end);
-        const startDate = dayjs(start);
-        let period = new Period(startDate, endDate);
-
-        let budgets = this.getBudgets();
-        let totalAmount = 0;
-        budgets.forEach((budget) => totalAmount += budget.overlappingAmount(period));
-        return totalAmount;
-        // if (budgets.length > 0) {
-        //     let budget = budgets[0];
-        //     return budget.overlappingAmount(period);
-        // }
-        // return 0;
+        return this.getBudgets()
+            .map((budget) => budget.overlappingAmount(new Period(dayjs(start), dayjs(end))))
+            .reduce((x, y) => x + y, 0);
     }
 
     getBudgets() {
         return undefined;
     }
-
 }
