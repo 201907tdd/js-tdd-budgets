3cac57e9a3f41b91003f9609e6f65b7362e28649
Joey Chen <jay.hatelove@gmail.com>
2020-10-24 13:55:33 +0800

    refactor: extract method overlappingDays()

--- src/budget_manager.js
+++ src/budget_manager.js
@@ -6,19 +6,24 @@ export class BudgetManager {
         if (budgets.length > 0) {
             const endDate = dayjs(end);
             const startDate = dayjs(start);
-            let lastDay = budgets[0].lastDay();
-            let firstDay = budgets[0].firstDay();
-            if (startDate.isAfter(lastDay)) {
-                return 0;
-            }
-            if (endDate.isBefore(firstDay)) {
-                return 0;
-            }
-            let overlappingStart = startDate.isAfter(firstDay) ? startDate : firstDay;
-            let overlappingEnd = endDate.isBefore(lastDay) ? endDate : lastDay;
-            return overlappingEnd.diff(overlappingStart, 'day') + 1;
+            let budget = budgets[0];
+            return this.overlappingDays(budget, startDate, endDate);
         }
         return 0;
+    }
+
+    overlappingDays(budget, startDate, endDate) {
+        let lastDay = budget.lastDay();
+        let firstDay = budget.firstDay();
+        if (startDate.isAfter(lastDay)) {
+            return 0;
+        }
+        if (endDate.isBefore(firstDay)) {
+            return 0;
+        }
+        let overlappingStart = startDate.isAfter(firstDay) ? startDate : firstDay;
+        let overlappingEnd = endDate.isBefore(lastDay) ? endDate : lastDay;
+        return overlappingEnd.diff(overlappingStart, 'day') + 1;
     }
 
     getBudgets() {
