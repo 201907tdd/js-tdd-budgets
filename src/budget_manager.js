5e3e07e59044236616caea2befe0f5c50c447d0c
Joey Chen <jay.hatelove@gmail.com>
2020-10-24 14:09:07 +0800

    style: clean up

--- src/budget_manager.js
+++ src/budget_manager.js
@@ -3,12 +3,13 @@ import {Period} from "./period";
 
 export class BudgetManager {
     totalAmount(start, end) {
+        const endDate = dayjs(end);
+        const startDate = dayjs(start);
+        let period = new Period(startDate, endDate);
+
         let budgets = this.getBudgets();
         if (budgets.length > 0) {
-            const endDate = dayjs(end);
-            const startDate = dayjs(start);
             let budget = budgets[0];
-            let period = new Period(startDate, endDate);
             return period.overlappingDays(budget);
         }
         return 0;
