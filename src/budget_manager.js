dc6b49b6ea39054936b6cff9046f69e085bc26b9
Joey Chen <jay.hatelove@gmail.com>
2020-10-24 10:29:23 +0800

    test: add failed test "no budgets"

---
+++ src/budget_manager.js
@@ -0,0 +1,5 @@
+export class BudgetManager {
+    totalAmount(start, end) {
+        return undefined;
+    }
+}
--- test/budget_manager.test.js
+++ test/budget_manager.test.js
@@ -1,3 +1,11 @@
-describe('total amount between period', function () {
+import {BudgetManager} from "../src/budget_manager";
 
+describe('total amount between period', function () {
+    it('no budgets', () => {
+        let budgetManager = new BudgetManager();
+        const start = new Date(2000, 3, 1);
+        const end = new Date(2000, 3, 1);
+        const totalAmount = budgetManager.totalAmount(start, end);
+        expect(totalAmount).toBe(0);
+    });
 });
